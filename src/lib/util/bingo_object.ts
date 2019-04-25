883ae719db31c46725ffd578e18a49eff33de49e
bingo <elvessun@gmail.com>
2019-04-25 15:59:48 +0800

    ...

--- src/lib/util/bingo_object.ts
+++
@@ -1,5 +0,0 @@
-export class BingoObject {
-    constructor() {
-
-    }
-}
--- src/test/testDescriptor.ts
+++ src/test/testDescriptor.ts
@@ -1,43 +1,109 @@
-import "reflect-metadata";
-import {Container, Inject, Service} from "typedi";
+export function BGField() {
+    return (target: Object, key: string): void => {
+        Object.defineProperty(target, key, {
+            get: function () {
+                return this['__fields'][key].value;
+            },
+            set: function (newValue) {
+                if (this['__fields'][key] === undefined) {
+                    this['__fields'][key] = {value: newValue, dirty: false};
+                }
+                else if (this['__fields'][key].value !== newValue) {
+                    this['__fields'][key].value = newValue;
+                    this['__fields'][key].dirty = true;
 
-@Service()
-class BeanFactory {
-    create() {
+                    let parent:DirtyModel = this['__parent'], self = this;
+                    while (parent) {
+                        parent['__dirty'] = true;
+                        parent['__fields'][self['__key']].dirty = true;
+                        self = parent;
+                        parent = parent['__parent'];
+                    }
+                }
+                else {
+                    this['__fields'][key].value = newValue;
+                }
+
+            }
+        });
+    };
+}
+
+interface IField {
+    value: any;
+    dirty: boolean;
+}
+
+class DirtyModel {
+    protected __fields: { [key: string]: IField } = {};
+    protected __dirty = false;
+    protected __parent: DirtyModel = null;
+    protected __key: string = '';
+
+    constructor(parent?: DirtyModel, key?: string) {
+        this.__parent = parent;
+        this.__key = key;
+    }
+
+    fields() {
+        return this.__fields;
+    }
+
+    dirtyFields(): string[] {
+        if (!this.__dirty) {
+            return [];
+        }
+
+        let ret = [];
+        for (let k in this.__fields) {
+            if (this.__fields[k].dirty) {
+                ret.push(k);
+            }
+        }
+        return ret;
+    }
+
+    isDirty() {
+        return this.__dirty;
+    }
+
+    serializeDirtyData() {
+    }
+
+    clearDirty() {
+        this.__dirty = false;
+    }
+
+}
+
+class TestModel extends DirtyModel {
+    @BGField() uid: number = 0;
+
+    constructor(parent: DirtyModel, key: string) {
+        super(parent, key);
     }
 }
 
-@Service()
-class SugarFactory {
-    create() {
+class ItemModel extends DirtyModel {
+    @BGField() uid: number = 0;
+    @BGField() id: number = 0;
+    @BGField() cnt: number = 0;
+    @BGField() testModel: TestModel = new TestModel(this, 'testModel');
+
+    constructor(parent: DirtyModel, key: string) {
+        super(parent, key);
     }
 }
 
-@Service()
-class WaterFactory {
-    constructor() {
-        console.log(1);
-    }
-
-    create() {
-    }
+class Role extends DirtyModel {
+    @BGField() uid: number = 0;
+    @BGField() name: string = '';
+    @BGField() itemModel: ItemModel = new ItemModel(this, 'itemModel');
 }
 
-class CoffeeMaker {
-
-    @Inject()
-    beanFactory: BeanFactory;
-
-    @Inject()
-    sugarFactory: SugarFactory;
-
-    @Inject()
-    waterFactory: WaterFactory;
-
-    make() {
-        this.beanFactory.create();
-        this.sugarFactory.create();
-        this.waterFactory.create();
-    }
-
-}
+let role = new Role();
+role.uid = 0;
+role.name = 'aa';
+role.itemModel['testModel'].uid = 1;
+console.log(role.dirtyFields());
+console.log(role.fields());
