b38e3b4d86a3f47a26ce6a84c6136eec2c6cc475
bingo <elvessun@gmail.com>
2019-04-25 21:11:35 +0800

    ...

--- src/test/testDescriptor.ts
+++ src/test/testDescriptor.ts
@@ -1,52 +1,41 @@
-export function BGField() {
+import {Test} from "tslint";
+
+export function BGField(dynamic: boolean = false) {
     return (target: Object, key: string): void => {
         Object.defineProperty(target, key, {
             get: function () {
-                return this['__fields'][key].value;
+                return this.__fields[key].value;
             },
             set: function (newValue) {
-                if (this['__fields'][key] === undefined) {
-                    this['__fields'][key] = {value: newValue, dirty: false};
+                if (this.__fields[key] === undefined) {
+                    this.__fields[key] = {value: newValue, dirty: false, dynamic: dynamic};
                 }
-                else if (this['__fields'][key].value !== newValue) {
-                    this['__fields'][key].value = newValue;
-                    this['__fields'][key].dirty = true;
-
-                    let parent:DirtyModel = this['__parent'], self = this;
-                    while (parent) {
-                        parent['__dirty'] = true;
-                        parent['__fields'][self['__key']].dirty = true;
-                        self = parent;
-                        parent = parent['__parent'];
-                    }
+                else if (this.__fields[key].value !== newValue) {
+                    this.__fields[key].value = newValue;
+                    this.__fields[key].dirty = true;
+                    this.makeDirty();
                 }
-                else {
-                    this['__fields'][key].value = newValue;
-                }
-
             }
         });
     };
 }
 
 interface IField {
-    value: any;
-    dirty: boolean;
+    value: number | string | BGValue;
+    dirty?: boolean;
+    dynamic?: boolean;
+    uid?: number;
 }
 
-class DirtyModel {
-    protected __fields: { [key: string]: IField } = {};
-    protected __dirty = false;
-    protected __parent: DirtyModel = null;
-    protected __key: string = '';
+abstract class BGValue {
+    __fields: { [key: string]: IField } = {};
+    __dirty = false;
+    __parent: BGValue = null;
+    __key: string = undefined;
 
-    constructor(parent?: DirtyModel, key?: string) {
+    constructor(parent?: BGValue, key?: string) {
         this.__parent = parent;
         this.__key = key;
-    }
-
-    fields() {
-        return this.__fields;
     }
 
     dirtyFields(): string[] {
@@ -63,47 +52,341 @@ class DirtyModel {
         return ret;
     }
 
-    isDirty() {
-        return this.__dirty;
+    makeDirty() {
+        this.__dirty = true;
+        let parent = this.__parent, self: BGValue = this;
+        while (parent && !parent.__dirty !== false) {
+            parent.__dirty = true;
+            if (self.__key !== undefined) {
+                parent.__fields[self.__key].dirty = true;
+            }
+            self = parent;
+            parent = parent.__parent;
+        }
+    }
+
+    clearDirty() {
+        if (!this.__dirty) {
+            return;
+        }
+        this.__dirty = false;
+        for (let k in this.__fields) {
+            let field = this.__fields[k];
+            if (field.dirty) {
+                field.dirty = false;
+                let value = field.value;
+                if (value instanceof BGValue) {
+                    value.clearDirty();
+                }
+            }
+        }
     }
 
     serializeDirtyData() {
+        // TODO
+    }
+}
+
+enum EDeltaOpt {
+    APPEND = 1,
+    DELETE = 2,
+    UPDATE = 3,
+    INSERT = 4
+}
+
+export class BGMap<T> extends BGValue {
+    private _data: { [key: string]: T } = {};
+    private _delta: any = {};
+    private _length: number = 0;
+
+    constructor(parent: BGValue, key: string, data ?: { [key: string]: T }) {
+        super(parent, key);
+        if (data) {
+            this._data = data;
+        }
+    }
+
+    get length() {
+        return this._length;
+    }
+
+    /**
+     * insert or update
+     * @param k
+     * @param v
+     */
+    set(k: number | string, v: T) {
+        if (v instanceof BGValue) {
+            v.__parent = this;
+        }
+
+        this._data[k] = v;
+
+        if (this._data[k] !== undefined) {
+            ++this._length;
+            this.updateDelta(EDeltaOpt.INSERT, k);
+        }
+        else {
+            this.updateDelta(EDeltaOpt.UPDATE, k);
+        }
+    }
+
+    get(k: number | string) {
+        return this._data[k];
+    }
+
+    remove(k: number | string) {
+        if (this._data[k] !== undefined) {
+            this.updateDelta(EDeltaOpt.DELETE, k);
+        }
+        delete this._data[k];
+        --this._length;
+    }
+
+    entries(): [string, T][] {
+        return Object.entries(this._data);
     }
 
     clearDirty() {
         this.__dirty = false;
+        for (let i in this._data) {
+            let v = this._data[i];
+            if (v instanceof BGValue) {
+                v.clearDirty();
+            }
+        }
     }
 
+    private updateDelta(opt: EDeltaOpt, k?: number | string) {
+        this.makeDirty();
+
+        // TODO
+        if (!this._delta[opt]) {
+            this._delta[opt] = [];
+        }
+
+        switch (opt) {
+            case EDeltaOpt.APPEND:
+                break;
+            case EDeltaOpt.DELETE:
+                break;
+            case EDeltaOpt.UPDATE:
+                break;
+            case EDeltaOpt.INSERT:
+                break;
+            default:
+                break;
+        }
+    }
 }
 
-class TestModel extends DirtyModel {
-    @BGField() uid: number = 0;
+export class BGArray<T extends BGValue | string | number> extends BGValue {
+    private _array: T[] = [];
+    private _delta: any = {};
 
-    constructor(parent: DirtyModel, key: string) {
+    constructor(parent: BGValue, key: string, data ?: T[]) {
         super(parent, key);
+        if (data) {
+            this._array.concat(data);
+        }
+    }
+
+    push(e: T) {
+        if (e instanceof BGValue) {
+            e.__parent = this;
+        }
+
+        this._array.push(e);
+        this.updateDelta(EDeltaOpt.APPEND, e);
+    }
+
+    get length() {
+        return this._array.length;
+    }
+
+    insert(pos: number, e: T) {
+        this.checkPos(pos);
+        if (e instanceof BGValue) {
+            e.__parent = this;
+        }
+        this._array = this._array.slice(0, pos).concat([e, ...this._array.slice(pos)]);
+        this.updateDelta(EDeltaOpt.INSERT, e);
+    }
+
+    print() {
+
+    }
+
+    find() {
+
+    }
+
+    findIndex() {
+
+    }
+
+    clear() {
+        for (let i = 0; i < this._array.length; ++i) {
+            this.updateDelta(EDeltaOpt.DELETE, null, i);
+        }
+        this._array = [];
+    }
+
+    // removeByValue(v: T) {
+    //     let idx = this.indexOf(v);
+    //     this.remove(idx);
+    // }
+
+    remove(pos: number) {
+        if (pos < 0 || pos >= this._array.length) {
+            return;
+        }
+
+        this._array.splice(pos, 1);
+        this.updateDelta(EDeltaOpt.DELETE, null, pos);
+    }
+
+    /**
+     * return new array not modify original
+     */
+    reverse(): T[] {
+        return this._array.reverse();
+    }
+
+    get(pos: number): T {
+        this.checkPos(pos);
+        return this._array[pos];
+    }
+
+    set(pos: number, v: T) {
+        if (v instanceof BGValue) {
+            v.__parent = this;
+        }
+        this.checkPos(pos);
+        this._array[pos] = v;
+        this.updateDelta(EDeltaOpt.UPDATE, v, pos);
+    }
+
+    private checkPos(pos: number) {
+        if (pos >= this._array.length || pos < 0) {
+            throw new Error("insert bingo array error, out of bound, pos=" + pos);
+        }
+    }
+
+    private updateDelta(opt: EDeltaOpt, obj?: T, pos?: number) {
+        this.makeDirty();
+
+        // TODO
+        if (!this._delta[opt]) {
+            this._delta[opt] = [];
+        }
+
+        switch (opt) {
+            case EDeltaOpt.APPEND:
+                break;
+            case EDeltaOpt.DELETE:
+                break;
+            case EDeltaOpt.UPDATE:
+                break;
+            case EDeltaOpt.INSERT:
+                break;
+            default:
+                break;
+        }
+    }
+
+    clearDirty() {
+        this.__dirty = false;
+        for (let i of this._array) {
+            if (i instanceof BGValue) {
+                i.clearDirty();
+            }
+        }
     }
 }
 
-class ItemModel extends DirtyModel {
+class TestInnest extends BGValue {
     @BGField() uid: number = 0;
-    @BGField() id: number = 0;
-    @BGField() cnt: number = 0;
-    @BGField() testModel: TestModel = new TestModel(this, 'testModel');
+    @BGField() test: number = 0;
 
-    constructor(parent: DirtyModel, key: string) {
-        super(parent, key);
+    constructor(uid, test) {
+        super();
+        this.uid = uid;
+        this.test = test;
     }
 }
 
-class Role extends DirtyModel {
+class TestInner extends BGValue {
+    @BGField() uid: number = 0;
+    @BGField() test: number = 0;
+    @BGField() array: BGArray<TestInnest> = new BGArray<TestInnest>(this, 'array');
+
+    constructor(uid, test) {
+        super();
+        this.uid = uid;
+        this.test = test;
+    }
+
+}
+
+class Role extends BGValue {
     @BGField() uid: number = 0;
     @BGField() name: string = '';
-    @BGField() itemModel: ItemModel = new ItemModel(this, 'itemModel');
+    @BGField() timeMap: BGMap<TestInner> = new BGMap<TestInner>(this, 'timeMap');
 }
 
 let role = new Role();
+
 role.uid = 0;
 role.name = 'aa';
-role.itemModel['testModel'].uid = 1;
 console.log(role.dirtyFields());
-console.log(role.fields());
+role.clearDirty();
+console.log(role.dirtyFields());
+
+role.timeMap.set(1, new TestInner(1, 3));
+role.timeMap.set(2, new TestInner(2, 4));
+
+for (let [k, v] of role.timeMap.entries()) {
+    let value = v as TestInner;
+    value.array.length;
+    value.uid += 10;
+}
+console.log(role.timeMap.get(1).uid);
+console.log(role.dirtyFields());
+role.clearDirty();
+console.log(role.dirtyFields());
+
+role.timeMap.get(1).array.push(new TestInnest(1, 0));
+role.timeMap.get(1).array.push(new TestInnest(2, 0));
+console.log(role.dirtyFields());
+role.clearDirty();
+console.log(role.dirtyFields());
+
+role.timeMap.get(1).array.get(0).test = 1;
+console.log(role.dirtyFields());
+role.clearDirty();
+console.log(role.dirtyFields());
+
+role.timeMap.get(1).array.insert(1, new TestInnest(1.5, 0));
+role.timeMap.get(1).array.insert(1, new TestInnest(1.6, 0));
+console.log(role.dirtyFields());
+role.clearDirty();
+console.log(role.dirtyFields());
+
+role.timeMap.get(1).array.remove(1);
+console.log(role.timeMap.get(1).array.length);
+console.log(role.dirtyFields());
+role.clearDirty();
+console.log(role.dirtyFields());
+
+console.log(role.dirtyFields());
+role.clearDirty();
+console.log(role.dirtyFields());
+
+console.log(role.dirtyFields());
+role.clearDirty();
+console.log(role.dirtyFields());
+
+console.log(role.dirtyFields());
+role.clearDirty();
+console.log(role.dirtyFields());
